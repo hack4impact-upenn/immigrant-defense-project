fc99f2cf7fc2d11d12c09ea1993db19f80201c3c
ivanesmeral <ivanes@seas.upenn.edu>
2018-10-16 15:17:20 -0400

    adding checklist blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -72,6 +72,7 @@ def create_app(config_name):
     from .admin import admin as admin_blueprint
     app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
-    # TODO: register checklist, url_prefix should be '/checklist'
+    from .checklist import checklist as checklist_blueprint
+    app.register_blueprint(checklist_blueprint, url_prefix='/checklist')
 
     return app
