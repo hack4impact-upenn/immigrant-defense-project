651387b3f98db57d5ad075d8ad9a29762941afce
Stephanie Shi <stephanie.y.shi@gmail.com>
2018-10-16 22:55:41 -0400

    Checklist blueprint (#3)
    * add frontend template for checklist
    * add checklist blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -72,4 +72,7 @@ def create_app(config_name):
     from .admin import admin as admin_blueprint
     app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
+    from .checklist import checklist as checklist_blueprint
+    app.register_blueprint(checklist_blueprint, url_prefix='/checklist')
+
     return app
