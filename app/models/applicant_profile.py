242b4d7ceb78d6313be29436566a39c1c1c27f79
siddharth-kumar <siddk@seas.upenn.edu>
2018-10-21 18:15:48 -0400

    added roles + applicant profile model (#6)
    * added roles + applicant Profile
    
    * Fix choice(roles) bug

---
+++ app/models/applicant_profile.py
@@ -0,0 +1,12 @@
+import random
+from faker import Faker
+from .. import db
+from sqlalchemy.orm import validates
+
+class ApplicantProfile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.relationship("User", back_populates="applicant_profile")
+    # PERSONAL INFO
+    phone_number = db.Column(db.String(15), index=True)
+    legal_advisor = db.Column(db.ForeignKey('applicant_profile.id'))
+    applicant = db.Column(db.ForeignKey('applicant_profile.id'))
