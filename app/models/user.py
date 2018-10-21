a2012ae4cbc5331790608b73e919454a7062128f
Katie Jiang <kajiang@seas.upenn.edu>
2018-10-21 18:14:32 -0400

    Fix choice(roles) bug

--- app/models/user.py
+++ app/models/user.py
@@ -67,7 +67,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    
+
     applicant_profile_id = db.Column(db.Integer, db.ForeignKey('applicant_profile.id'))
     applicant_profile = db.relationship("ApplicantProfile", uselist = False, back_populates="user")
 
@@ -193,13 +193,14 @@ class User(UserMixin, db.Model):
 
         seed()
         for i in range(count):
+            role = choice(roles)
             u = User(
                 first_name=fake.first_name(),
                 last_name=fake.last_name(),
                 email=fake.email(),
                 password='password',
                 confirmed=True,
-                role=choice(roles),
+                role=role,
                 **kwargs)
             if role.name == 'User':
                 u.applicant_profile = ApplicantProfile.generate_fake()
