acd1d1e032af6021644a84e030defbbcd6782bac
Stephanie Shi <stephanie.y.shi@gmail.com>
2018-10-21 17:56:37 -0400

    increase length of description in checklist model

--- app/models/checklist.py
+++ app/models/checklist.py
@@ -4,4 +4,4 @@ class ChecklistItem(db.Model):
     __tablename__ = 'checklist_item'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64))
-    description = db.Column(db.String(64))
+    description = db.Column(db.String(256))
