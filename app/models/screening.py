8a135898b6da7106917a9b9fe3e1c77411829ce3
Sarah Raines <saraines@seas.upenn.edu>
2018-10-25 02:07:04 -0400

    update screening.py

--- app/models/screening.py
+++ app/models/screening.py
@@ -3,4 +3,12 @@ from .. import db
 class ScreeningQuestion(db.Model):
     __tablename__ = 'screening_question'
     id = db.Column(db.Integer, primary_key=True)
-    question = db.Column(db.String(256))
+    question = db.Column(db.Text)
+    answers = db.relationship('ScreeningAnswer', backref='question', lazy='dynamic')
+
+class ScreeningAnswer(db.Model):
+    __tablename__ = 'screening_answer'
+    id = db.Column(db.Integer, primary_key=True)
+    applicant = db.Column(db.Integer, db.ForeignKey('applicant_profile.id'))
+    question = db.Column(db.Integer, db.ForeignKey('screening_question.id'))
+    answer = db.Column(db.Text)
