7bc331c12f50cad05831e3ab8f667bf2502832e7
Sarah Raines <saraines@seas.upenn.edu>
2018-10-25 02:02:55 -0400

    added model to store applicant responses to screening questions

--- app/models/applicant_profile.py
+++ app/models/applicant_profile.py
@@ -4,9 +4,11 @@ from .. import db
 from sqlalchemy.orm import validates
 
 class ApplicantProfile(db.Model):
+    __tablename__ = 'applicant_profile'
     id = db.Column(db.Integer, primary_key=True)
     user = db.relationship("User", back_populates="applicant_profile")
     # PERSONAL INFO
     phone_number = db.Column(db.String(15), index=True)
     legal_advisor = db.Column(db.ForeignKey('applicant_profile.id'))
     applicant = db.Column(db.ForeignKey('applicant_profile.id'))
+    screening_responses = db.relationship('ScreeningAnswer', backref='applicant', lazy='dynamic')
