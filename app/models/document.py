63fc3aad4a88a90e1c17b33d3b748f81f3865c17
Stephanie Shi <stephanie.y.shi@gmail.com>
2018-10-27 23:02:30 -0400

    Update document.py

--- app/models/document.py
+++ app/models/document.py
@@ -5,4 +5,4 @@ class Document(db.Model):
     __tablename__ = 'document'
     user_id = db.Column(db.Integer, primary_key=True)
     document_urls = db.Column(db.Text)
-    checklist_items = db.relationship('UserChecklistItem', backref='documents', lazy=True)
+    checklist_items = db.relationship('UserChecklistItem', backref='document', lazy=True)
