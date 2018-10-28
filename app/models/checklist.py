17d5d64508640bb05719915679bdbdc4b4d9e1d2
Stephanie Shi <stephanie.y.shi@gmail.com>
2018-10-28 17:38:18 -0400

    add generate_fake() functions for DefaultChecklistItem and UserChecklistItem (#23)
    * add generate_fake() functions for DefaultChecklistItem and UserChecklistItem
    
    * remove unused imports

--- app/models/checklist.py
+++ app/models/checklist.py
@@ -7,6 +7,25 @@ class DefaultChecklistItem(db.Model):
     title = db.Column(db.String(64))
     description = db.Column(db.Text)
 
+    @staticmethod
+    def generate_fake(count=10):
+        from sqlalchemy.exc import IntegrityError
+        from random import seed
+        from faker import Faker
+
+        fake = Faker()
+        seed()
+        for i in range(count):
+            item = DefaultChecklistItem(
+                title=fake.word(),
+                description=fake.sentence()
+            )
+            db.session.add(item)
+            try:
+                db.session.commit()
+            except IntegrityError:
+                db.session.rollback()
+
 
 class UserChecklistItem(db.Model):
     __tablename__ = 'user_checklist_item'
@@ -16,6 +35,27 @@ class UserChecklistItem(db.Model):
     completed = db.Column(db.Boolean, index=True, default=False)
     applicant_profile_id = db.Column(db.ForeignKey('applicant_profile.id'))
     documents = db.relationship('Document', backref='user_checklist_item', lazy=True)
+
+    # TODO: may be changed once relations between applicant_profile and user
+    # are hashed out
+    @staticmethod
+    def generate_fake(count=5):
+        from sqlalchemy.exc import IntegrityError
+        from random import seed
+        from faker import Faker
+
+        fake = Faker()
+        seed()
+        for i in range(count):
+            item = UserChecklistItem(
+                title=fake.word(),
+                description=fake.sentence()
+            )
+            db.session.add(item)
+            try:
+                db.session.commit()
+            except IntegrityError:
+                db.session.rollback()
 
     def __repr__(self):
         return '<User Checklist Item: Title = {}, Description = {}, Completed = {}, Document = {}>'.format(self.title, self.description, self.completed, self.document)
