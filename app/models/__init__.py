6d7248e8448f25e6cc4537b5a52795efb27e6bd0
Stephanie Shi <stephanie.y.shi@gmail.com>
2018-10-27 22:32:59 -0400

    rename documents to document and alphabetize model imports (#14)

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,8 +4,8 @@ module (as opposed to just their python files)
 """
 from .applicant_profile import *
 from .checklist import * # noqa
+from .document import *
 from .reminder import *  # noqa
 from .user import *  # noqa
-from .miscellaneous import *  # noqa
 from .screening import *
-from .documents import *
+from .miscellaneous import *  # noqa
