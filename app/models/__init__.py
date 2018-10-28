fe2dd0a4f8df6f538b74734184d834fc938961f0
Stephanie Shi <stephanie.y.shi@gmail.com>
2018-10-27 22:40:32 -0400

    fix model import order

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,7 +5,7 @@ module (as opposed to just their python files)
 from .applicant_profile import *
 from .checklist import * # noqa
 from .document import *
-from .reminder import *  # noqa
-from .user import *  # noqa
-from .screening import *
 from .miscellaneous import *  # noqa
+from .reminder import *  # noqa
+from .screening import *
+from .user import *  # noqa
