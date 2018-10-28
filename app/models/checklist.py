962f8343aa6b31766681fa89602e399c64c57e05
Ivan Esmeral <ivanesmeral@Ivans-MacBook-Pro.local>
2018-10-27 22:43:04 -0400

    making relationship between checklist items and applicant profile

--- app/models/checklist.py
+++ app/models/checklist.py
@@ -15,7 +15,10 @@ class UserChecklistItem(db.Model):
     description = db.Column(db.Text)
     completed = db.Column(db.Boolean, index=True, default=False)
     applicant_profile_id = db.Column(db.ForeignKey('applicant_profile.id'))
+<<<<<<< HEAD
     documents = db.relationship('Document', backref='user_checklist_item', lazy=True)
 
     def __repr__(self):
         return '<User Checklist Item: Title = {}, Description = {}, Completed = {}, Document = {}>'.format(self.title, self.description, self.completed, self.document)
+=======
+>>>>>>> fb34cbf... making relationship between checklist items and applicant profile
