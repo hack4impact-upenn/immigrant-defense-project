b1a71c46e61ffd650a3ae65c34c799e9112c3625
Stephanie Shi <stephanie.y.shi@gmail.com>
2018-10-28 19:13:04 -0400

    add decorators

--- app/advisor/views.py
+++ app/advisor/views.py
@@ -3,13 +3,14 @@ from flask import (
     render_template,
 )
 from flask_login import login_required
+from app.decorators import advisor_required
 
 advisor = Blueprint('advisor', __name__)
 
 
-# TODO: add advisor_required decorator
 @advisor.route('/', methods=['GET', 'POST'])
 @login_required
+@advisor_required
 def index():
     """Advisor dashboard page."""
     return render_template('advisor/index.html')
--- app/screener/views.py
+++ app/screener/views.py
@@ -3,13 +3,14 @@ from flask import (
     render_template,
 )
 from flask_login import login_required
+from app.decorators import screener_required
 
 screener = Blueprint('screener', __name__)
 
 
-# TODO: add screener_required decorator
 @screener.route('/', methods=['GET', 'POST'])
 @login_required
+@screener_required
 def index():
     """Screener dashboard page."""
     return render_template('screener/index.html')
