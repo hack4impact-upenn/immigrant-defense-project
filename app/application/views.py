19fcbd749acffa7eb1bf85fa4d798afc26f86cbf
ninatchang <ninatchang@berkeley.edu>
2018-11-08 12:29:00 -0500

    added buttons and function but needs to change a lot of stuff

--- app/application/views.py
+++ app/application/views.py
@@ -33,3 +33,30 @@ def view(user_id):
     if not user.application:
         return redirect(404)
     return render_template('application/profile.html', application=user.application)
+
+@login_required
+@application.route('<int:user_id>/change_status_to_complete')
+def change_status_to_complete(user_id):
+    application = Application.query.join(User).filter(User.id == user_id).get(user_id)
+    if application is None:
+        abort(404)
+    old_title = default_checklist_item.title
+    form = DefaultChecklistItemForm()
+    type = "Edit"
+    if form.validate_on_submit():
+        default_checklist_item.title = form.title.data
+        default_checklist_item.description = form.description.data
+        try:
+            db.session.commit()
+            flash(
+                'Default checklist item {} successfully changed.'.format(
+                    old_title), 'form-success')
+        except IntegrityError:
+            db.session.rollback()
+            flash('Error Occurred. Please try again.', 'form-error')
+        return render_template(
+            'checklist/edit_checklist_item.html', form=form, type=type)
+    form.title.data = default_checklist_item.title
+    form.description.data = default_checklist_item.description
+    return render_template(
+        'checklist/edit_checklist_item.html', form=form, type=type)
