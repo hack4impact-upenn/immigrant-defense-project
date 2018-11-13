8eaa7d6e4b32c082ebcd1a590cf530678e7f122b
Steven Bursztyn <stevenbursztyn@users.noreply.github.com>
2018-11-13 14:15:57 -0500

    Match applications -> legal advisor (#48)
    * good progress :)
    
    * push buggy
    
    * changes
    
    * yay it works now :)
    
    * update stage

--- app/application/views.py
+++ app/application/views.py
@@ -4,13 +4,14 @@ from flask_login import current_user, login_required
 from flask_rq import get_queue
 
 from app import db
-from app.models import Application, User
+from app.models import Application, User, Stage
+from app.application.forms import AssignAdvisorForm
 
 application = Blueprint('application', __name__)
 
 
 @login_required
-@application.route('/')
+@application.route('/', methods=['GET', 'POST'])
 def index():
     if current_user.is_applicant():
         return redirect(url_for('application.view'), current_user.id)
@@ -23,7 +24,23 @@ def index():
     else:
         return redirect(404)
         # applications = Application.query.filter_by(legal_advisor=current_user.id).all()
-    return render_template('application/dashboard.html', applicants=applicants)
+
+    advisor_form = AssignAdvisorForm()
+    if advisor_form.validate_on_submit():
+        for app_id in advisor_form.applicant_ids.data.split(','):
+            user = User.query.filter_by(id=app_id).first()
+            data = Application.query.filter_by(id=user.application_id).first()
+            data.legal_advisor = advisor_form.advisor.data
+            data.stage = Stage.MATCHED_ADVISOR
+            db.session.add(data)
+        db.session.commit()
+        flash('Successfully assigned advisors!', 'form-success')
+        return redirect(url_for('application.index'))
+
+    return render_template(
+        'application/dashboard.html', 
+        applicants=applicants,
+        advisor_form=advisor_form)
 
 
 @login_required
