949d9f55fe9797a491b4788bf042413519d1ec52
Katie Jiang <kajiang@seas.upenn.edu>
2018-11-13 18:28:25 -0500

    Remove question type

--- app/models/survey.py
+++ app/models/survey.py
@@ -1,20 +1,9 @@
 from .. import db
 
 
-class QuestionType():
-    TEXT = 'text'
-    PARAGRAPH = 'paragraph'
-    CHECKBOX = 'checkbox'
-    RADIO = 'radio'
-    DATE = 'date'
-    NUMBER = 'number'
-    DECIMAL = 'decimal'
-
-
 class SurveyQuestion(db.Model):
     __tablename__ = 'survey_question'
     id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String, nullable=False)
     content = db.Column(db.String, unique=True, nullable=False)
     description = db.Column(db.String, nullable=False)
     # options = db.Column(db.PickleType)
@@ -22,33 +11,15 @@ class SurveyQuestion(db.Model):
     responses = db.relationship('SurveyResponse', backref='question', lazy='dynamic')
 
     @staticmethod
-    def type_choices():
-        return [(type, type) for type in SurveyQuestion.types()]
-
-    @staticmethod
-    def types():
-        return [
-            QuestionType.TEXT,
-            QuestionType.PARAGRAPH,
-            QuestionType.CHECKBOX,
-            QuestionType.RADIO,
-            QuestionType.DATE,
-            QuestionType.NUMBER,
-            QuestionType.DECIMAL,
-        ]
-
-    @staticmethod
     def generate_fake(count=5):
         from sqlalchemy.exc import IntegrityError
         from faker import Faker
         from random import choice
         fake = Faker()
-        types = SurveyQuestion.types()
 
         for i in range(count):
             question = SurveyQuestion(
                 content=f'Question #{i + 1}',
-                type=choice(types),
                 description=fake.sentence(),
                 screen=choice([True, False]),
             )
