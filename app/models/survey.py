bffa76cf0dc78a89eed104c157a04e23b39036d0
Katie Jiang <kajiang@seas.upenn.edu>
2018-11-14 00:18:26 -0500

    Survey question/response/option model (#55)
    * Remove question type
    
    * Add SurveyOption model, edit SurveyQuestion/Response models, add fake data

--- app/models/survey.py
+++ app/models/survey.py
@@ -1,56 +1,30 @@
+from sqlalchemy.exc import IntegrityError
+from faker import Faker
+import random
+
 from .. import db
-
-
-class QuestionType():
-    TEXT = 'text'
-    PARAGRAPH = 'paragraph'
-    CHECKBOX = 'checkbox'
-    RADIO = 'radio'
-    DATE = 'date'
-    NUMBER = 'number'
-    DECIMAL = 'decimal'
 
 
 class SurveyQuestion(db.Model):
     __tablename__ = 'survey_question'
     id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String, nullable=False)
+    rank = db.Column(db.Integer, unique=True, nullable=False)
     content = db.Column(db.String, unique=True, nullable=False)
     description = db.Column(db.String, nullable=False)
-    # options = db.Column(db.PickleType)
-    screen = db.Column(db.Boolean, default=False, nullable=False)
-    responses = db.relationship('SurveyResponse', backref='question', lazy='dynamic')
+    options = db.relationship('SurveyOption', backref='question', lazy='dynamic')
 
     @staticmethod
-    def type_choices():
-        return [(type, type) for type in SurveyQuestion.types()]
-
-    @staticmethod
-    def types():
-        return [
-            QuestionType.TEXT,
-            QuestionType.PARAGRAPH,
-            QuestionType.CHECKBOX,
-            QuestionType.RADIO,
-            QuestionType.DATE,
-            QuestionType.NUMBER,
-            QuestionType.DECIMAL,
-        ]
+    def next_rank():
+        return SurveyQuestion.query.count() + 1
 
     @staticmethod
     def generate_fake(count=5):
-        from sqlalchemy.exc import IntegrityError
-        from faker import Faker
-        from random import choice
         fake = Faker()
-        types = SurveyQuestion.types()
-
         for i in range(count):
             question = SurveyQuestion(
                 content=f'Question #{i + 1}',
-                type=choice(types),
                 description=fake.sentence(),
-                screen=choice([True, False]),
+                rank=SurveyQuestion.next_rank(),
             )
             db.session.add(question)
             try:
@@ -59,9 +33,50 @@ class SurveyQuestion(db.Model):
                 db.session.rollback()
 
 
+class SurveyOptionAction():
+    STOP = -1           # survey stops; applicant cannot proceed to make an account
+    CONTINUE = 0        # default
+
+
+class SurveyOption(db.Model):
+    __tablename__ = 'survey_option'
+    id = db.Column(db.Integer, primary_key=True)
+    question_id = db.Column(db.Integer, db.ForeignKey('survey_question.id'))
+    content = db.Column(db.String, nullable=False)
+    next_action = db.Column(db.Integer, nullable=False, default=SurveyOptionAction.CONTINUE)
+
+    @staticmethod
+    def generate_fake(question, count=4):
+        for i in range(random.randint(2, count)):
+            option = SurveyOption(
+                question_id=question.id,
+                content=f'Option #{i + 1} for {question.content}',
+            )
+            db.session.add(option)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+
+
 class SurveyResponse(db.Model):
     __tablename__ = 'survey_response'
     id = db.Column(db.Integer, primary_key=True)
     application_id = db.Column(db.Integer, db.ForeignKey('application.id'))
-    question_id = db.Column(db.Integer, db.ForeignKey('survey_question.id'))
-    content = db.Column(db.Text)
+    question_content = db.Column(db.String, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+    @staticmethod
+    def generate_fake(application):
+        for question in SurveyQuestion.query.all():
+            options = SurveyOption.query.filter_by(question_id=question.id).all()
+            response = SurveyResponse(
+                application_id=application.id,
+                question_content=question.content,
+                content=random.choice(options).content,
+            )
+            db.session.add(response)
+            try:
+                db.session.commit()
+            except IntegrityError:
+                db.session.rollback()
