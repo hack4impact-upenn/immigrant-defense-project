51c2c892b4e756abfceb327f47e2a4dbe02c37e1
Sarah Raines <saraines@seas.upenn.edu>
2018-11-28 22:30:38 -0500

    edit profile for screeners and advisors

--- app/advisor/views.py
+++ app/advisor/views.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, render_template, flash
+from flask_login import current_user, login_required
 
 from app.decorators import advisor_required
+from app.advisor.forms import EditAdvisorInfoForm
+from app.models import User
+from .. import db
 
 advisor = Blueprint('advisor', __name__)
 
@@ -12,3 +15,29 @@ advisor = Blueprint('advisor', __name__)
 def index():
     """Advisor dashboard page."""
     return render_template('advisor/index.html')
+
+@advisor.route('/edit', methods=['GET', 'POST'])
+@login_required
+@advisor_required
+def edit_profile():
+    """Advisor edit page."""
+    user = User.query.filter_by(id=current_user.id).first()
+    if user is None:
+        abort(404)
+    form = EditAdvisorInfoForm()
+    if form.validate_on_submit():
+        user.location = form.location.data
+        user.clemency_familiarity = form.clemency_familiarity.data
+        user.law_experience = form.law_experience.data
+        user.bio = form.bio.data
+        user.languages = form.languages.data
+        db.session.commit()
+        flash(
+            'Profile for screener {} successfully updated.'.format(
+                user.full_name()), 'form-success')
+    form.location.data = user.location
+    form.clemency_familiarity.data = user.clemency_familiarity
+    form.law_experience.data = user.law_experience
+    form.bio.data = user.bio
+    form.languages.data = user.languages
+    return render_template('advisor/edit.html', user=user, form=form)
--- app/screener/views.py
+++ app/screener/views.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, render_template, flash
+from flask_login import current_user, login_required
 
 from app.decorators import screener_required
+from app.models import User
+from app.screener.forms import EditScreenerInfoForm
+from .. import db
 
 screener = Blueprint('screener', __name__)
 
@@ -12,3 +15,29 @@ screener = Blueprint('screener', __name__)
 def index():
     """Screener dashboard page."""
     return render_template('screener/index.html')
+
+@screener.route('/edit', methods=['GET', 'POST'])
+@login_required
+@screener_required
+def edit_profile():
+    """Screener edit page."""
+    user = User.query.filter_by(id=current_user.id).first()
+    if user is None:
+        abort(404)
+    form = EditScreenerInfoForm()
+    if form.validate_on_submit():
+        user.location = form.location.data
+        user.immigrant_experience = form.immigrant_experience.data
+        user.crime_experience = form.crime_experience.data
+        user.bio = form.bio.data
+        user.languages = form.languages.data
+        db.session.commit()
+        flash(
+            'Profile for screener {} successfully updated.'.format(
+                user.full_name()), 'form-success')
+    form.location.data = user.location
+    form.immigrant_experience.data = user.immigrant_experience
+    form.crime_experience.data = user.crime_experience
+    form.bio.data = user.bio
+    form.languages.data = user.languages
+    return render_template('screener/edit.html', user=user, form=form)
